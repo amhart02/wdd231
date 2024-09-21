c0989cb42c2500096c5866cf54e2f5be0f92aa16
amhart02 <har20010@byui.edu>
2024-09-21 15:24:26 -0600

    finished up everything

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+function parkInfoTemplate(info) {
+    return `<a href="/" class="hero_banner_title">${info.name}</a>
+    <p class="hero_banner_subtitle">
+    <span>${info.designation}</span>
+    <span>${info.states}</span>
+    </p>`; 
+}
+
+document.querySelector("title").innerHTML = parkData.fullName;
+document.querySelector(".hero_banner_content").innerHTML = parkInfoTemplate(parkData);
+document.querySelector(".hero_banner img").src = parkData.images[0].url;
